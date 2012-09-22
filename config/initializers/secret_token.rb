edfd5c7656f94c4eb7692983f6e5a19d7ecafd66
rightgo09 <skyarrow09@gmail.com>
2012-09-22 11:21:58 +0900

    Create new Application "Rails3RecipeBook"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3RecipeBook::Application.config.secret_token = 'bccad08c3899c9dbabe1548fd66a5e28be7c40d87f06778e4f6e5cc117dd41554ef309e1c8dd64ff7b4015bd181e6412b31bb46e89f8f636524a11c3dd97d9d8'
